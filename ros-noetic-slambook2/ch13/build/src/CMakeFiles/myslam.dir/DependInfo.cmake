338291158774386fc650c23a06db9583d520cbe9
Your Name <you@example.com>
2023-12-23 11:50:10 +0800

    initial summit

---
+++ ros-noetic-slambook2/ch13/build/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/work/study/slambook2/ch13/src/backend.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/backend.cpp.o"
+  "/home/work/study/slambook2/ch13/src/camera.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/camera.cpp.o"
+  "/home/work/study/slambook2/ch13/src/config.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/config.cpp.o"
+  "/home/work/study/slambook2/ch13/src/dataset.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/dataset.cpp.o"
+  "/home/work/study/slambook2/ch13/src/feature.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/feature.cpp.o"
+  "/home/work/study/slambook2/ch13/src/frame.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/frame.cpp.o"
+  "/home/work/study/slambook2/ch13/src/frontend.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/frontend.cpp.o"
+  "/home/work/study/slambook2/ch13/src/map.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/map.cpp.o"
+  "/home/work/study/slambook2/ch13/src/mappoint.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/mappoint.cpp.o"
+  "/home/work/study/slambook2/ch13/src/viewer.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/viewer.cpp.o"
+  "/home/work/study/slambook2/ch13/src/visual_odometry.cpp" "/home/work/study/slambook2/ch13/build/src/CMakeFiles/myslam.dir/visual_odometry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  "myslam_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/suitesparse"
+  "../include"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ros-noetic-slambook2/ch13/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/work/study/slambook2/ch13/test
+# Build directory: /home/work/study/slambook2/ch13/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_triangulation "test_triangulation")
+set_tests_properties(test_triangulation PROPERTIES  _BACKTRACE_TRIPLES "/home/work/study/slambook2/ch13/test/CMakeLists.txt;6;ADD_TEST;/home/work/study/slambook2/ch13/test/CMakeLists.txt;0;")
